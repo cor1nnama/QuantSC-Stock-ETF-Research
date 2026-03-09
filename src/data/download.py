5a5ae31fe5a0ffd33b932b953cb2c2c986323bdc
karanmaliekkal <karanmaliekkal@gmail.com>
2026-03-09 16:47:20 -0700

    added Healthcare, Energy, consumer staples, real estate, and communication services

--- src/data/download.py
+++ src/data/download.py
@@ -9,7 +9,13 @@ tickers = {"Tech": ["AAPL", "MSFT", "GOOG", "NVDA", "ADBE", "ORCL", "INTC", "TXN
            "Financials": ["JPM", "BAC", "C", "GS", "MS", "BRK-A", "V", "MA", "AXP", "BLK"], 
            "Consumer_Disc": ["TSLA", "NKE", "HD", "LOW", "SBUX", "MCD", "TGT", "TJX", "LULU", "SONO"], 
            "Industrials": ["GE", "CAT", "BA", "MMM", "HON", "ITW", "UNP", "LMT", "FDX", "RTX"], 
-           "Utilities": ["NEE", "DUK", "SO", "D", "AEP", "EXC", "XEL", "SRE", "PEG", "CNP"]}
+           "Utilities": ["NEE", "DUK", "SO", "D", "AEP", "EXC", "XEL", "SRE", "PEG", "CNP"],
+           "Healthcare": ["LLY", "JNJ", "ABBV", "MRK", "PFE", "AMGN", "GILD", "ISRG", "HCA", "UNH"],
+           "Energy": ["XOM","CVX","COP","EOG","SLB","OXY","DVN","FANG","LNG","HAL"],
+           "Consumer_Staples": ["PG","KO","PEP","WMT","COST","CL","KMB","GIS","HSY","MKC"],
+           "Real_Estate": ["AMT","PLD","EQIX","O","SPG","WELL","DLR","AVB","EQR","VICI"],
+           "Communication_Services": ["META","NFLX","DIS","TMUS","VZ","T","CHTR","FOXA","PARA","GOOG"]
+          }
 
 
 # Dictionary to store downloaded data for each ticker
